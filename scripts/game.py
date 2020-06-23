ae41ebe25dbcc6f573656d0f12e2de943d897d15
Aghil K P <aghilkp91@gmail.com>
2020-06-23 10:33:12 +0200

    Ai vs minimax

--- scripts/game.py
+++ scripts/game.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import time
 
 from game_ai import Game_AI
 from minimax import Game_Minimax
@@ -12,7 +13,7 @@ class Game():
     PLAYER1 = 'PLAYER 1'
     PLAYER2 = 'PLAYER 2'
 
-    def __init__(self, board_width, board_height, should_use_ai, ai_genes, should_use_minimax, background=(245,245,245), line_color=(220, 220, 220), line_thickness=8, shape_thickness=10, player_o_color=(255, 69, 0),player_x_color=(41, 41, 41)):
+    def __init__(self, board_width, board_height, should_use_ai, ai_genes, should_use_minimax, comp_vs_comp, background=(245,245,245), line_color=(220, 220, 220), line_thickness=8, shape_thickness=10, player_o_color=(255, 69, 0),player_x_color=(41, 41, 41)):
         """
         Initialize game state
         """
@@ -29,6 +30,16 @@ class Game():
         self._PLAYER_SHAPES[Game.PLAYER1] = Game.SHAPE_X
         self._PLAYER_SHAPES[Game.PLAYER2] = Game.SHAPE_O
 
+        self.comp_vs_comp = comp_vs_comp
+
+        self.no_of_games = 0
+        self.player_o_wins = 0
+        self.player_x_wins = 0
+        self.tie = 0
+
+        self.player_1 = None
+        self.player_2 = None
+
         self._should_reset = True
         self._reset()
 
@@ -37,10 +48,12 @@ class Game():
             defensive_gene = float(ai_genes[1])
             random_gene = float(ai_genes[2])
             self._game_ai = Game_AI(aggressive_gene, defensive_gene, random_gene)
-        elif bool(should_use_minimax):
-            self._game_ai = Game_Minimax()
         else:
             self._game_ai = None
+        if bool(should_use_minimax):
+            self._game_minimax = Game_Minimax()
+        else:
+            self._game_minimax = None
 
     def _set_clicked_position(self, coordinate):
         """
@@ -66,7 +79,7 @@ class Game():
         """
         if self._is_over and self._has_shown_message:
             self._should_reset = True
-        elif self._player_1_can_click:
+        elif self._game_minimax is None and self._player_1_can_click:
             position = self._set_clicked_position(coordinate)
             if self._player_1_filled[position[0]][position[1]] == 1 or self._player_2_filled[position[0]][position[1]] == 1:
                 return
@@ -109,11 +122,28 @@ class Game():
             y = self._clicked_position[0] * self._BOARD_HEIGHT / 3.0 + ((1 / 2.0) * (self._BOARD_HEIGHT / 3.0))
             pygame.draw.circle(screen, self.player_o_color, (int(x), int(y)), int((3 / 8.0) * (self._BOARD_HEIGHT / 3.0)), self._SHAPE_THICKNESS)
 
+    def get_no_of_games(self):
+        return self.no_of_games
+
+    def get_results(self):
+        res = {}
+        res["Evolutionary Algorithm"] = self.player_o_wins
+        res["Minimax"] = self.player_x_wins
+        res["Tie"] = self.tie
+        return res
+
     def _show_winner(self, screen, font='Arial', font_size=50, font_color=(0, 0, 255)):
         """
         Displays the winner of the game
         """
         font_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0,255));
+        if self._winning_player == Game.PLAYER1:
+            self.player_x_wins = self.player_x_wins + 1
+        elif self._winning_player == Game.PLAYER2:
+            self.player_o_wins = self.player_o_wins + 1
+        else:
+            self.tie = self.tie + 1
+
         message = self._winning_player + ' WINS!!!' if self._winning_player is not None else 'IT\'S A TIE!'
         message_x = self._BOARD_WIDTH / 2.0 - ((len(message) / 78.0) * self._BOARD_WIDTH)
         message_y = self._BOARD_HEIGHT / 2.0 - ((1 / 20.0) * self._BOARD_HEIGHT)
@@ -130,15 +160,18 @@ class Game():
             self._current_player = Game.PLAYER1 if self._current_player == Game.PLAYER2 else Game.PLAYER2
             self._clicked_position = None
         if self._is_over and not self._has_shown_message:
+            self.no_of_games = self.no_of_games + 1
             self._show_winner(screen)
             self._has_shown_message = True
+            self._should_reset = True
 
-    def _reset(self, starting_player=PLAYER2):
+    def _reset(self):
         """
         Reset the game state
         """
         self._is_over = False
         self._has_shown_message = False
+        starting_player = random.choice([Game.PLAYER1, Game.PLAYER2])
         self._current_player = starting_player
         self._winning_player = None
         self._player_1_can_click = self._current_player == Game.PLAYER1
@@ -147,6 +180,7 @@ class Game():
 
         self._player_1_filled = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
         self._player_2_filled = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        time.sleep(5)
 
     def _check_if_game_over(self):
         """
@@ -184,6 +218,7 @@ class Game():
         if self._should_reset:
             self._reset()
             self._should_reset = False
+            self._init_board(screen)
         elif self._is_over:
             return
         elif self._check_if_game_over():
@@ -202,3 +237,16 @@ class Game():
             self._player_1_can_click = True
             self._clicked_position = None
             self._current_player = Game.PLAYER1
+            time.sleep(1)
+        elif self._game_minimax is not None and self._player_1_can_click:
+            position = self._game_minimax.choose_position(self._player_1_filled, self._player_2_filled)
+            if position is not None:
+                self._player_1_filled[position[0]][position[1]] = 1
+                self._clicked_position = position
+                self._current_player = Game.PLAYER1
+                self._draw_shape(screen)
+            self._player_1_can_click = False
+            self._player_2_can_click = True
+            self._clicked_position = None
+            self._current_player = Game.PLAYER2
+            time.sleep(1)
--- scripts/run_game.py
+++ scripts/run_game.py
@@ -6,7 +6,7 @@ import sys
 from game import Game
 
 
-def main(board_width, board_height, title, fps, should_use_ai, ai_genes, should_use_minimax):
+def main(board_width, board_height, title, fps, should_use_ai, ai_genes, should_use_minimax, run_loop):
     """
     Main game loop
     """
@@ -15,24 +15,40 @@ def main(board_width, board_height, title, fps, should_use_ai, ai_genes, should_
     pygame.display.set_caption(title)
     pygame.font.init()
     clock = pygame.time.Clock()
-    game = Game(board_width, board_height, should_use_ai, ai_genes, should_use_minimax)
+    comp_vs_comp = False
+    if (should_use_minimax and should_use_ai):
+        comp_vs_comp = True
+    game = Game(board_width, board_height, should_use_ai, ai_genes, should_use_minimax, comp_vs_comp)
+    curr_loop = 0
+    run_maximum = int(run_loop) if run_loop else -1
 
     while True:
+        no_of_games = game.get_no_of_games()
+        if run_maximum != -1 and no_of_games > run_maximum:
+            results = game.get_results()
+            print_result(results)
+            pygame.quit()
+            sys.exit(0)
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                results = game.get_results()
+                print_result(results)
                 pygame.quit()
                 sys.exit(0)
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.MOUSEBUTTONDOWN and comp_vs_comp == False:
                 game.notify_click(event.pos)
-
         game.draw(screen)
         game.update(screen)
 
         pygame.display.update()
         clock.tick(fps)
 
+def print_result(results):
+    for key, value in results.items():
+        print(key, ' : ', value)
 
-def run(should_use_ai, should_use_minimax):
+
+def run(should_use_ai, should_use_minimax, run_loop):
     """
     Run the program using the cli inputs
     """
@@ -49,8 +65,9 @@ def run(should_use_ai, should_use_minimax):
         AI_GENES = (float(ai_genes_dict['aggressive_gene']), float(ai_genes_dict['defensive_gene']), float(ai_genes_dict['random_gene']))
 
     SHOULD_USE_MINIMAX = bool(should_use_minimax)
+    print(run_loop)
 
-    main(BOARD_WIDTH, BOARD_HEIGHT, TITLE, FPS, SHOULD_USE_AI, AI_GENES, SHOULD_USE_MINIMAX)
+    main(BOARD_WIDTH, BOARD_HEIGHT, TITLE, FPS, SHOULD_USE_AI, AI_GENES, SHOULD_USE_MINIMAX, run_loop)
 
 
 if __name__ == "__main__":
@@ -58,9 +75,11 @@ if __name__ == "__main__":
     parser.add_argument('-e', '--use-ai', dest='use_ai', action='store_true', help='Enable the evolutionary algorithm and instead play a 2-person game')
     parser.add_argument('-m', '--use-minimax', dest='use_minimax', action='store_true',
                         help='Enable the minimax and instead play a 2-person game')
+    parser.add_argument('-n', '--run-loop', dest='run_loop',
+                        help='Run the game for given number of times')
     args = parser.parse_args()
 
     try:
-        run(args.use_ai, args.use_minimax)
+        run(args.use_ai, args.use_minimax, args.run_loop)
     except Exception as e:
         print(e)
